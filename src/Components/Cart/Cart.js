83248a33225bb2f28a4b3cfe713abc5ea618416c
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-08 18:11:14 +0600

    add to cart and remove from cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
-const Cart = () => {
+const Cart = ({ cart, handleRemoveItem }) => {
     return (
         <div>
             <h3>Order summary</h3>
-        </div>
+            <h5>Order Quantity: {cart.length}</h5>
+            {
+                cart.map(tshirt => <p
+                    key={tshirt._id}
+                >
+                    {tshirt.name}
+                    <button onClick={() => handleRemoveItem(tshirt)}> X</button>
+                </p>)
+            }
+        </div >
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
@@ -6,18 +6,39 @@ import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = tshirt => {
+        // console.log(tshirt);
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+            alert('t-shirt already added')
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            // alert('successfully added');
+        }
+    }
+    const handleRemoveItem = tshirt => {
+        // console.log(tshirt);
+        const remaining = cart.filter(ts => ts._id !== tshirt._id);
+        setCart(remaining);
+    }
     return (
         <div className='home-container'>
             <div className="tshirt-container">
                 {
                     tshirts.map(tshirt => <TShirt
-                        key={tshirt.id}
+                        key={tshirt._id}
                         tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     ></TShirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart cart={cart}
+                    handleRemoveItem={handleRemoveItem}
+                ></Cart>
             </div>
         </div>
     );
