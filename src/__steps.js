c6006493602606d1ad96c81dd8ca07726096ffd1
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-08 13:38:17 +0600

    explore rect router in t-shirt mania

---
+++ src/__steps.js
@@ -0,0 +1,10 @@
+/* 
+1. npm i react-router-dom
+2. create a router using createBrowserRouter
+3. add RouterProvider
+4. Create a lot of component
+5. create Main layout with some common part and one changing part and one changing part based on the route
+6. set main layout at the root of the route
+7. set children routes
+8. set links on the header components
+*/
